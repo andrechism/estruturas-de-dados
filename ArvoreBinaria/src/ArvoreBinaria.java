68ba484991aef583f66d493814d3f77036866d5c
Wolney Freitas <wolneyhqf@gmail.com>
2020-05-22 09:11:19 -0300

    Aula árvore binária de busca

--- ArvoreBinaria/src/ArvoreBinaria.java
+++ ArvoreBinaria/src/ArvoreBinaria.java
@@ -1,7 +1,7 @@
 
 public class ArvoreBinaria {
-
-	private No raiz;
+	
+	private No raiz = null;
 	
 	public boolean isVazia() {
 		return raiz == null;
@@ -20,7 +20,10 @@ public class ArvoreBinaria {
 		}
 		int alturaEsquerda = getAltura(no.esquerda);
 		int alturaDireita = getAltura(no.direita);
-		return alturaEsquerda > alturaDireita ? alturaEsquerda + 1 : alturaDireita + 1;
+		if(alturaEsquerda > alturaDireita) {
+			return alturaEsquerda + 1;
+		}
+		return alturaDireita + 1;
 	}
 	
 	public int getQuantidadeElementos() {
@@ -44,7 +47,7 @@ public class ArvoreBinaria {
 		no.dado = dado;
 		no.direita = null;
 		no.esquerda = null;
-		if(raiz == null) {
+		if(isVazia()) {
 			raiz = no;
 			return true;
 		}
@@ -69,71 +72,6 @@ public class ArvoreBinaria {
 		return true;
 	}
 	
-	public boolean remover(int dado) {
-		if(isVazia()) {
-			return false;
-		}
-		No atual = raiz;
-		No anterior = null;
-		while(atual != null) {
-			if(dado == atual.dado) {
-				if(atual == raiz) {
-					raiz = removeAtual(atual);
-				}else {
-					if(anterior.direita == atual) {
-						anterior.direita = removeAtual(atual);
-					}else {
-						anterior.esquerda = removeAtual(atual);
-					}
-				}
-				return true;
-			}
-			anterior = atual;
-			if(dado > atual.dado) {
-				atual = atual.direita;
-			}else {
-				atual = atual.esquerda;
-			}
-		}
-		return true;
-	}
-	
-	private No removeAtual(No atual) {
-		if(atual.esquerda == null) {
-			return atual.direita;
-		}
-		No no1 = atual;
-		No no2 = atual.esquerda;
-		while(no2.direita != null) {
-			no1 = no2;
-			no2 = no2.direita;
-		}
-		if(no1 != atual) {
-			no1.direita = no2.esquerda;
-			no2.esquerda = atual.esquerda;
-		}
-		no2.direita = atual.direita;
-		return no2;
-	}
-	
-	public boolean consultar(int dado) {
-		if(isVazia()) {
-			return false;
-		}
-		No atual = raiz;
-		while(atual != null) {
-			if(dado == raiz.dado) {
-				return true;
-			}
-			if(dado > atual.dado) {
-				atual = atual.direita;
-			}else {
-				atual = atual.esquerda;
-			}
-		}
-		return false;
-	}
-	
 	public void imprimir() {
 		if(isVazia()) {
 			System.out.println("Vazia");
@@ -154,5 +92,5 @@ public class ArvoreBinaria {
 		No esquerda;
 		No direita;
 	}
-	
+
 }
---
+++ ArvoreBinaria/src/Programa.java
@@ -0,0 +1,25 @@
+
+public class Programa {
+	
+	public static void main(String[] args) {
+		ArvoreBinaria a = new ArvoreBinaria();
+		
+		a.inserir(8);
+		a.inserir(4);
+		a.inserir(2);
+		a.inserir(6);
+		a.inserir(1);
+		a.inserir(3);
+		a.inserir(5);
+		a.inserir(12);
+		a.inserir(10);
+		a.inserir(14);
+		a.inserir(11);
+		a.inserir(13);
+		a.inserir(15);
+		
+		a.imprimir();
+		
+	}
+
+}
